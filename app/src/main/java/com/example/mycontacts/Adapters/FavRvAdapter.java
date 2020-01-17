f533596a5ee0d71edd4ab14de2a878d5ce320610
Mohammad <hacker.m2h@gmail.com>
2020-01-17 21:20:17 +0330

    set call button for favourites fragment

--- app/src/main/java/com/example/mycontacts/Adapters/FavRvAdapter.java
+++ app/src/main/java/com/example/mycontacts/Adapters/FavRvAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,10 +20,12 @@ public class FavRvAdapter extends RecyclerView.Adapter<FavRvAdapter.ViewHolder> 
     private Context context;
     private LayoutInflater inflater;
     private List<ModelContacts> listContacts;
+    private OnBtnCallFavListener onBtnCallFavListener;
 
-    public FavRvAdapter(Context context, List<ModelContacts> listContacts) {
+    public FavRvAdapter(Context context, List<ModelContacts> listContacts, OnBtnCallFavListener onBtnCallFavListener) {
         this.context = context;
         this.listContacts = listContacts;
+        this.onBtnCallFavListener = onBtnCallFavListener;
     }
 
     @NonNull
@@ -30,7 +33,7 @@ public class FavRvAdapter extends RecyclerView.Adapter<FavRvAdapter.ViewHolder> 
     public FavRvAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.item_fav,parent,false);
-        ViewHolder viewHolder = new ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view,onBtnCallFavListener);
         return viewHolder;
     }
 
@@ -60,16 +63,38 @@ public class FavRvAdapter extends RecyclerView.Adapter<FavRvAdapter.ViewHolder> 
         return size;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView fav_name,fav_number;
+        OnBtnCallFavListener onBtnCallFavListener;
+        Button call;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, final OnBtnCallFavListener onBtnCallFavListener) {
             super(itemView);
 
             fav_name = itemView.findViewById(R.id.fav_name);
             fav_number = itemView.findViewById(R.id.fav_number);
+            call = itemView.findViewById(R.id.btn_call_fav);
+
+            this.onBtnCallFavListener = onBtnCallFavListener;
+            call.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBtnCallFavListener.onBtnCallClick(listContacts.get(getAdapterPosition()).getNumber());
+                }
+            });
 
         }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    public interface OnBtnCallFavListener{
+
+        void onBtnCallClick(String number);
+
     }
 }
--- app/src/main/java/com/example/mycontacts/fragments/FragmentFav.java
+++ app/src/main/java/com/example/mycontacts/fragments/FragmentFav.java
@@ -1,5 +1,9 @@
 package com.example.mycontacts.fragments;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +11,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,7 +21,7 @@ import com.example.mycontacts.Adapters.FavRvAdapter;
 import com.example.mycontacts.MyPreferenceManager;
 import com.example.mycontacts.R;
 
-public class FragmentFav extends Fragment {
+public class FragmentFav extends Fragment implements FavRvAdapter.OnBtnCallFavListener {
 
     private RecyclerView recyclerView;
     FavRvAdapter adapter;
@@ -38,18 +43,32 @@ public class FragmentFav extends Fragment {
         setFavAdapter();
     }
 
-    
+
     private void setFavAdapter(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         RecyclerView.LayoutManager layoutManager = linearLayoutManager;
         recyclerView.setLayoutManager(layoutManager);
 
-        adapter = new FavRvAdapter(getContext(), MyPreferenceManager.getInstance(getActivity()).getContactList());
+        adapter = new FavRvAdapter(getContext(), MyPreferenceManager.getInstance(getActivity()).getContactList(),this);
         recyclerView.setAdapter(adapter);
 
     }
 
     private void findViews(View view){
         recyclerView = view.findViewById(R.id.rv_fav);
+    }
+
+    @Override
+    public void onBtnCallClick(String number) {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.CALL_PHONE},123);
+
+        }
+
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        getActivity().startActivity(intent);
+
     }
 }
