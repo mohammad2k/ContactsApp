42ed0be6d5e43e90cbb617956a3e98f9446d3fc4
Mohammad <hacker.m2h@gmail.com>
2020-01-17 11:44:06 +0330

    add put and get contacts to prefrence

--- app/src/main/java/com/example/mycontacts/MyPreferenceManager.java
+++ app/src/main/java/com/example/mycontacts/MyPreferenceManager.java
@@ -3,7 +3,13 @@ package com.example.mycontacts;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.example.mycontacts.models.ModelCalls;
+import com.example.mycontacts.models.ModelContacts;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class MyPreferenceManager {
 
@@ -24,6 +30,25 @@ public class MyPreferenceManager {
         return instance;
     }
     //================================================================
+
+
+    public void putContactList(List<ModelContacts> contactList){
+
+        Gson gson = new Gson();
+        String contactListJson = gson.toJson(contactList);
+        editor.putString("contact_list",contactListJson);
+        editor.apply();
+
+    }
+
+    public List<ModelContacts> getContactList(){
+
+        String contactListJson = sharedPreferences.getString("contact_list",null);
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<ModelContacts>>() {}.getType();
+        return gson.fromJson(contactListJson,type);
+
+    }
 
 
 
